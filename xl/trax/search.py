feiying/exaile

commit e5ea6ba7b4e4e3839ef5a9667e00867d5e2f6a67

This fixes exaile/exaile#176: Ignore diacritics for search (#261)
* This fixes exaile/exaile#176: Ignore diacritics for search

* Use unidecode.normalize for track searches which ignore diacritics as opposed to unidecode

xl/trax/search.py
@@ -26,6 +26,8 @@
 
 import time
 import re
+import unicodedata
+
 
 __all__ = ['TracksMatcher', 'search_tracks']
 
@@ -64,14 +66,17 @@ class _Matcher(object):
             if type(vals) != list:
                 vals = [vals]
             for item in vals:
-                try:
+                if item != None:
                     item = self.lower(item)
-                except:
-                    pass
-                if self._matches(item):
-                    return True
-            else:
-                return False
+                    try:
+                        item = self.lower(unicodedata.normalize(
+                            'NFKD', item).encode('ASCII', 'ignore'))
+                    except:
+                        pass
+                    if self._matches(item):
+                        return True
+                    else:
+                        return False
 
     def _matches(self, value):
         raise NotImplementedError
@@ -231,7 +236,12 @@ class TracksMatcher(object):
         """
         self.case_sensitive = case_sensitive
         self.keyword_tags = keyword_tags or []
-        tokens = self.__tokenize_query(search_string)
+        if isinstance(search_string, unicode):
+            tokens = self.__tokenize_query(
+                unicodedata.normalize(
+                    'NFKD', search_string).encode('ASCII','ignore'))
+        else:
+            tokens = self.__tokenize_query(search_string)
         tokens = self.__red(tokens)
         tokens = self.__optimize_tokens(tokens)
         self.matchers = self.__tokens_to_matchers(tokens)

commit 1c3f53838b6882c94261f5f95b09424150fa1309

Fix unit test bugs introduced in e5ea6ba7b4e4e3839ef5a9667e00867d5e2f6a67

xl/trax/search.py
@@ -61,21 +61,19 @@ class _Matcher(object):
         if self.tag.startswith("__"):
             if self._matches(vals):
                 return True
-            return False
         else:
             if type(vals) != list:
                 vals = [vals]
             for item in vals:
-                if item != None:
+                if item is not None:
                     try:
                         item = item.decode('ascii')
                     except:
                         item = shave_marks(item)
                     item = self.lower(item)
-                    if self._matches(item):
-                        return True
-                    else:
-                        return False
+                if self._matches(item):
+                    return True
+        return False
 
     def _matches(self, value):
         raise NotImplementedError